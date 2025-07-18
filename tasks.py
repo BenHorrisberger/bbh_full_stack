db2273ea947fd4182500428bef906554d6843c38
BenHorrisberger <benhorrisberger@gmail.com>
2025-07-18 13:42:35 -0400

    added default values and default path to the list-viable task

--- tasks.py
+++ tasks.py
@@ -5,13 +5,13 @@ from apps.read_aircraft_data.read_data import read_aircraft_data
 
 
 @task(help={
-    "path": "file path to the .csv",
-    "distance": "the distance in miles required",
-    "seats": "the number of seats required",
-    "bags": "the number of bags required"
+    "path": "file path to the .csv, default=aircraft_data/Aircraft.csv",
+    "distance": "the distance in miles required, default=0",
+    "seats": "the number of seats required, default=0",
+    "bags": "the number of bags required, default=0"
 
 })
-def list_viable(c, path=None, distance=0, seats=0, bags=0):
+def list_viable(c, path="aircraft_data/Aircraft.csv", distance=0, seats=0, bags=0):
     """List viable aircraft based on a range, seats, bags requirements"""
 
     task_aircraft_list = read_aircraft_data(path)
